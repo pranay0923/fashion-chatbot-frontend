85f96d58ce061352de0274cf7f0e9df61b5b78c8
Mudigonda Pranay Kumar <73039685+pranay0923@users.noreply.github.com>
2025-07-29 13:20:10 +0530

    app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ st.markdown("""
       color: #197177;
       border-radius: 29px 19px 22px 29px;
       border: 1px solid #cdf2fb31;
-      text-align: left;
+      text-align: right;
       font-family: inherit;
     }
     .assistant-bubble {
