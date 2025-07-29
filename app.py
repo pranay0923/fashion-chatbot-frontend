ad9b14d3ec76a7104149cb6b96e08fa11c875cdb
Mudigonda Pranay Kumar <73039685+pranay0923@users.noreply.github.com>
2025-07-29 12:20:25 +0530

    app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ st.markdown("""
 
 
 # --- UI Layout ---
-st.image("https://github.com/pranay0923/fashion-chatbot-frontend/blob/main/WhatsApp%20Image%202025-07-29%20at%2012.03.57%20PM.jpeg", use_column_width=True)
+st.image("https://github.com/pranay0923/fashion-chatbot-frontend/blob/main/WhatsApp%20Image%202025-07-29%20at%2012.03.57%20PM.jpeg", use_container_width=True)
 st.title("Ask our Fashion AI anything")
 st.write("Suggestions on what to ask Our AI")
 
