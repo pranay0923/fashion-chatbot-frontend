6d9d580f56ed87e22033ac034f7ef4edfd7c7716
Mudigonda Pranay Kumar <73039685+pranay0923@users.noreply.github.com>
2025-07-29 12:56:54 +0530

    app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import json
 
 # --- Page Configuration ---
 st.set_page_config(
-    page_title="Fashion Chatbot",
+    page_title="Fashion AI",
     page_icon="https://raw.githubusercontent.com/pranay0923/fashion-chatbot-frontend/main/WhatsApp%20Image%202025-07-29%20at%2012.03.57%20PM.jpeg",
-    layout="center"
+    layout="wide"
 )
+
 
 # --- CSS for the Look and Feel ---
 st.markdown("""
